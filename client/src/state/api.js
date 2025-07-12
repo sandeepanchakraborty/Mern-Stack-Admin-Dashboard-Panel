1c6e7b3d12dcea0f4817f925ba6ce547ea8963c4
sandeepanchakraborty <sandeepanchakraborty123@gmail.com>
2025-07-12 20:05:43 +0530

    Your descriptive commit message here

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -4,7 +4,7 @@ import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const api=createApi({
     baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_BASE_URL}),
     reducerPath:"adminApi",
-    tagTypes:["User","Products","Customers"],
+    tagTypes:["User","Products","Customers","Transactions","Geography"],
     endpoints:(build)=>({
         getUser:build.query({
             query:(id)=>`general/user/${id}`,
@@ -26,9 +26,17 @@ export const api=createApi({
       }),
       providesTags: ["Transactions"],
     }),
+    getGeography: build.query({
+        query:()=>"client/geography",
+        providesTags:["Geography"],
+    }),
     }),
 });
 
 export const {
-    useGetUserQuery, useGetProductsQuery,useGetCustomersQuery,useGetTransactionsQuery,
+    useGetUserQuery,
+     useGetProductsQuery,
+     useGetCustomersQuery,
+     useGetTransactionsQuery,
+     useGetGeographyQuery,
 }=api;
