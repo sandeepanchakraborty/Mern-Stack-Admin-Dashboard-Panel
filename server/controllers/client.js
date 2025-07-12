1ad77e2486d72ef91eb4643fedcd4d07d35ad9ce
sandeepanchakraborty <sandeepanchakraborty123@gmail.com>
2025-07-12 20:05:33 +0530

    Your descriptive commit message here

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -84,7 +84,18 @@ export const getGeography=async(req,res)=>{
     const users=await User.find();
     const mappedLocations=users.reduce((acc,{country})=>{
       const countryISO3=getCountryIso3(country);
-    })
+      if(!acc[countryISO3]){
+        acc[countryISO3]=0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    },{});
+
+    const formattedLocations=Object.entries(mappedLocations).map(
+      ([country,count])=>{
+        return {id:country,value:count};
+      }
+    )
 
   }catch(error){
         res.status(404).json({message: "error.message"});
