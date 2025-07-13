8f4241b75e491d3afba84a9649e88ea123986241
sandeepanchakraborty <sandeepanchakraborty123@gmail.com>
2025-07-13 18:19:14 +0530

    Your descriptive commit message here

--- client/src/scenes/geography/index.jsx
+++ client/src/scenes/geography/index.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import {Box,useTheme} from "@mui/material";
-import { useGetGeographyQuery } from '../../state/api';
-import Header from '../../components/Header';
-import {ResponsiveChoropleth} from "@nivo/geo";
-import {geoData} from "../../state/geoData";
-
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetGeographyQuery } from "../../state/api";
+import Header from "../../components/Header";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geoData } from "../../state/geoData";
 
 const Geography = () => {
-    const theme=useTheme();
-    const {data}=useGetGeographyQuery();
-    console.log('data',data);
+  const theme = useTheme();
+  const { data } = useGetGeographyQuery();
+  console.log("data", data);
 
-  return <div>Geography</div>
-}
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="GEOGRAPHY" subtitle="Find where your users are located." />
+      <Box
+        mt="40px"
+        height="75vh"
+        border={`1px solid ${theme.palette.secondary[200]}`}
+        borderRadius="4px"
+      >
+        {}
+      </Box>
+    </Box>
+  );
+};
 
-export default Geography
+export default Geography;
