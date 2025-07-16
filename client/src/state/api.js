a54ed319b0fa635476fb3683a9995753124797a0
sandeepanchakraborty <sandeepanchakraborty123@gmail.com>
2025-07-16 22:42:10 +0530

    Your descriptive commit message here

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,6 +1,5 @@
 import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-
 export const api=createApi({
     baseQuery:fetchBaseQuery({baseUrl:process.env.REACT_APP_BASE_URL}),
     reducerPath:"adminApi",
@@ -14,7 +13,7 @@ export const api=createApi({
         "Performance",
         "Dashboard",
     ],
-    endpoints:(build)=>({
+    endpoints:(build)=>(({
         getUser:build.query({
             query:(id)=>`general/user/${id}`,
             provideTags:["User"]
@@ -28,45 +27,44 @@ export const api=createApi({
             providesTags:["Customers"],
         }),
         getTransactions: build.query({
-      query: ({ page, pageSize, sort, search }) => ({
-        url: "client/transactions",
-        method: "GET",
-        params: { page, pageSize, sort, search },
-      }),
-      providesTags: ["Transactions"],
-    }),
-    getGeography: build.query({
-        query:()=>"client/geography",
-        providesTags:["Geography"],
-    }),
-    getSales:build.query({
-        query:()=>"sales/sales",
-        providesTags:["Sales"],
-    }),
-    getAdmins:build.query({
-        query:()=>"management/admins",
-        providesTags:["Admins"],
-    }),
-    getUserPerformance:build.query({
-        query:(id)=>`mananagement/performance/${id}"`,
-        providesTags:["Performance"]
-    }),
-     getDashboard: build.query({
-      query: () => "general/dashboard",
-      providesTags: ["Dashboard"],
-    }),
-
-    }),
+            query: ({ page, pageSize, sort, search }) => ({
+                url: "client/transactions",
+                method: "GET",
+                params: { page, pageSize, sort, search },
+            }),
+            providesTags: ["Transactions"],
+        }),
+        getGeography: build.query({
+            query:()=>"client/geography",
+            providesTags:["Geography"],
+        }),
+        getSales:build.query({
+            query:()=>"sales/sales",
+            providesTags:["Sales"],
+        }),
+        getAdmins:build.query({
+            query:()=>"management/admins",
+            providesTags:["Admins"],
+        }),
+        getUserPerformance:build.query({
+            query:(id)=>`management/performance/${id}`,
+            providesTags:["Performance"]
+        }),
+        getDashboard: build.query({
+            query: () => "general/dashboard",
+            providesTags: ["Dashboard"],
+        }),
+    })),
 });
 
 export const {
     useGetUserQuery,
-     useGetProductsQuery,
-     useGetCustomersQuery,
-     useGetTransactionsQuery,
-     useGetGeographyQuery,
-     useGetSalesQuery,
-     useGetAdminsQuery,
-     useGetUserPerformanceQuery,
-     useGetDashboardQuery,
+    useGetProductsQuery,
+    useGetCustomersQuery,
+    useGetTransactionsQuery,
+    useGetGeographyQuery,
+    useGetSalesQuery,
+    useGetAdminsQuery,
+    useGetUserPerformanceQuery,
+    useGetDashboardQuery,
 }=api;
